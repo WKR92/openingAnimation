52ea21eeb97f304bffcad02c72baf219133ea443
Wiktor <65229344+WKR92@users.noreply.github.com>
2021-06-06 16:38:33 +0200

    first

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { gsap } from 'gsap';
 import { CSSRulePlugin } from "gsap/CSSRulePlugin";
+import throttle from 'lodash.throttle';
+import debounce from 'lodash.debounce';
 gsap.registerPlugin(CSSRulePlugin);
 
 const rule = CSSRulePlugin.getRule(".box3:before");
@@ -11,215 +13,102 @@ const tl = gsap.timeline()
     .to(rule, {cssRule: {left: '100%'}, duration: .5}, .75)
     .to(['.box3'], {scaleX: 0, transformOrigin: 'left', duration: .5}, 2.25)
     .to(['.box2'], {scaleX: 0, transformOrigin: 'right', duration: .5}, 4)
-    .fromTo('.leftBox', {x: '-100%', duration: .5}, {x: 0}, 5.15)
-    .fromTo('.bgDiv', {opacity: 0}, {opacity: 1, duration: 3}, 5.5)
+    .to(['.scene'], {opacity: 1}, 3)
+    .to(['.box1'], {backgroundColor: "transparent"}, 3)
+    .fromTo('.leftBox', {x: '-100%', duration: .5}, {x: 0}, 3)
+    .fromTo('.bgDiv', {opacity: 0}, {opacity: 1, duration: 3}, 2)
+    .to('.main', {background: '#485563',
+        background: '-webkit-linear-gradient(to right, #29323c, #485563)', 
+        background: 'linear-gradient(to right, #29323c, #485563)', duration: 1}, 4)
     .to('.box1', {clearProps: "transform"}, 4)
-    // .to('.box1Text', {color: '#fff', scale: 1.2, duration: 3}, 5.5)
 
 
-    const inside = document.querySelector('.inside');
-    inside.addEventListener('click', function(e) {
-        console.log('click inside1')            
-
-        const inside = document.querySelector('.inside');
-        const inside2 = document.querySelector('.inside2');
-        const inside3 = document.querySelector('.inside3');
-        const inside4 = document.querySelector('.inside4');
-
-        inside.classList.remove("state-one");
-        inside2.classList.remove("state-two");
-        inside3.classList.remove("state-three");
-        inside4.classList.remove("state-four");
-
-        inside.classList.add("state-two");
-        inside2.classList.add("state-three");
-        inside3.classList.add("state-four");
-        inside4.classList.add("state-one");
-
-
-        console.log(inside)
-        console.log(inside2)
-        console.log(inside3)
-        console.log(inside4)
-    })
-
-    const inside4 = document.querySelector('.inside4');
-    inside4.addEventListener('click', function(e) {
-        console.log('click inside4')
-
-        const inside = document.querySelector('.inside');
-        const inside2 = document.querySelector('.inside2');
-        const inside3 = document.querySelector('.inside3');
-        const inside4 = document.querySelector('.inside4');
-
-        inside.classList.remove("state-two");
-        inside2.classList.remove("state-three");
-        inside3.classList.remove("state-four");
-        inside4.classList.remove("state-one");
-
-        inside.classList.add("state-three");
-        inside2.classList.add("state-four");
-        inside3.classList.add("state-one");
-        inside4.classList.add("state-two");
-
-        console.log(inside)
-        console.log(inside2)
-        console.log(inside3)
-        console.log(inside4)
-    })
-
-    const inside3 = document.querySelector('.inside3');
-    inside3.addEventListener('click', function(e) {
-        console.log('click inside3')
-
-        const inside = document.querySelector('.inside');
-        const inside2 = document.querySelector('.inside2');
-        const inside3 = document.querySelector('.inside3');
-        const inside4 = document.querySelector('.inside4');
-
-        inside.classList.remove("state-three");
-        inside2.classList.remove("state-four");
-        inside3.classList.remove("state-one");
-        inside4.classList.remove("state-two");
-
-        inside.classList.add("state-four");
-        inside2.classList.add("state-one");
-        inside3.classList.add("state-two");
-        inside4.classList.add("state-three");
-
-        console.log(inside)
-        console.log(inside2)
-        console.log(inside3)
-        console.log(inside4)
-    })
-
-    const inside2 = document.querySelector('.inside2');
-    inside2.addEventListener('click', function(e) {
-        console.log('click inside2')
-
-        const inside = document.querySelector('.inside');
-        const inside2 = document.querySelector('.inside2');
-        const inside3 = document.querySelector('.inside3');
-        const inside4 = document.querySelector('.inside4');
-
-        inside.classList.remove("state-four");
-        inside2.classList.remove("state-one");
-        inside3.classList.remove("state-two");
-        inside4.classList.remove("state-three");
-
-        inside.classList.add("state-one");
-        inside2.classList.add("state-two");
-        inside3.classList.add("state-three");
-        inside4.classList.add("state-four");
-
-        console.log(inside)
-        console.log(inside2)
-        console.log(inside3)
-        console.log(inside4)
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const box1 = document.querySelector('.box1');
-    box1.addEventListener('click', function(e) {
-        console.log('click inside1')            
-
-        const box1 = document.querySelector('.box1');
-        const box4 = document.querySelector('.box4');
-        const box5 = document.querySelector('.box5');
-        const box6 = document.querySelector('.box6');
-
-        box1.classList.remove("state-one");
-        box4.classList.remove("state-two");
-        box5.classList.remove("state-three");
-        box6.classList.remove("state-four");
-
-        box1.classList.add("state-two");
-        box4.classList.add("state-three");
-        box5.classList.add("state-four");
-        box6.classList.add("state-one");
-    })
-
-    const box6 = document.querySelector('.box6');
-    box6.addEventListener('click', function(e) {
-
-
-        const box1 = document.querySelector('.box1');
-        const box4 = document.querySelector('.box4');
-        const box5 = document.querySelector('.box5');
-        const box6 = document.querySelector('.box6');
-
-        box1.classList.remove("state-two");
-        box4.classList.remove("state-three");
-        box5.classList.remove("state-four");
-        box6.classList.remove("state-one");
-
-        box1.classList.add("state-three");
-        box4.classList.add("state-four");
-        box5.classList.add("state-one");
-        box6.classList.add("state-two");
+    let rotateX = 0
+    const cube = document.querySelector('.cube');
+    cube.addEventListener('click', (event)=>{
+        event.preventDefault();
         
-    })
-
-    const box5 = document.querySelector('.box5');
-    box5.addEventListener('click', function(e) {
-
-        const box1 = document.querySelector('.box1');
-        const box4 = document.querySelector('.box4');
-        const box5 = document.querySelector('.box5');
-        const box6 = document.querySelector('.box6');
-
-        box1.classList.remove("state-three");
-        box4.classList.remove("state-four");
-        box5.classList.remove("state-one");
-        box6.classList.remove("state-two");
-
-        box1.classList.add("state-four");
-        box4.classList.add("state-one");
-        box5.classList.add("state-two");
-        box6.classList.add("state-three");
-    })
-
-    const box4 = document.querySelector('.box4');
-    box4.addEventListener('click', function(e) {
-
-        const box1 = document.querySelector('.box1');
-        const box4 = document.querySelector('.box4');
-        const box5 = document.querySelector('.box5');
-        const box6 = document.querySelector('.box6');
-
-        box1.classList.remove("state-four");
-        box4.classList.remove("state-one");
-        box5.classList.remove("state-two");
-        box6.classList.remove("state-three");
-
-        box1.classList.add("state-one");
-        box4.classList.add("state-two");
-        box5.classList.add("state-three");
-        box6.classList.add("state-four");
-    })
-
-    window.addEventListener('click', e => {
-        console.log(e.target)
+        cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX -90) + 'deg)';
+        rotateX = rotateX - 90;
+        console.log(rotateX);
     })
 
 
+    // working on scroll
+    // let rotateX = 0
+    // window.onscroll = function() {
+    //     const cube = document.querySelector('.cube');        
+        
+    //     if (this.oldScroll < this.scrollY) {
+    //         cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX - 90) + 'deg)';
+    //         rotateX = rotateX - 90;
+    //         console.log(rotateX);
+    //     } else {
+    //         cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX + 90) + 'deg)';
+    //         rotateX = rotateX + 90;
+    //         console.log(rotateX);
+    //     }
+    //     console.log(rotateX)
+    //     this.oldScroll = this.scrollY;
+    // }
 
-    
+//     function rotateCube(event) {
+//         event.preventDefault();
+//         console.log('start')
+//         const cube = document.querySelector('.cube'); 
+//         if (this.oldScroll < this.scrollY) {
+//             cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX - 90) + 'deg)';
+//             rotateX = rotateX - 90;
+//         } else {
+//             cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX + 90) + 'deg)';
+//             rotateX = rotateX + 90;
+//         }
+//         console.log(rotateX)
+//         this.oldScroll = this.scrollY;
+
+//         console.log('koniec')
+//     }
+
+// window.addEventListener('scroll', throttle(rotateCube, 1000));
+
+
+
+let contactLeftValue = "100%"
+const contactNavBtn = document.querySelector('.contactNavBtn');
+contactNavBtn.addEventListener('click', () => {
+
+    const contactContainer = document.querySelector('.contactContainer');
+    if(contactLeftValue === "20%"){
+        contactContainer.style['left'] = "100%";
+        contactLeftValue = "100%";
+    } else {
+        contactContainer.style['left'] = "20%";
+        contactLeftValue = "20%";
+    }    
+})
+
+const homeNavBtn = document.querySelector('.homeNavBtn');
+homeNavBtn.addEventListener('click', () => {
+    const cube = document.querySelector('.cube');
+    cube.style.transform = "translateZ(-40vh) rotateX(0deg)"
+    rotateX = 0
+})
+
+const processNavBtn = document.querySelector('.processNavBtn');
+processNavBtn.addEventListener('click', () => {
+    const cube = document.querySelector('.cube');
+    cube.style.transform = "translateZ(-50vh) rotateY(-90deg)"
+})
+
+const portfolioNavBtn = document.querySelector('.portfolioNavBtn');
+portfolioNavBtn.addEventListener('click', () => {
+    const cube = document.querySelector('.cube');
+    cube.style.transform = "translateZ(-50vh) rotateY(90deg)"
+})
+
+const whyWeNavBtn = document.querySelector('.whyWeNavBtn');
+whyWeNavBtn.addEventListener('click', () => {
+    const cube = document.querySelector('.cube');
+    cube.style.transform = "translateZ(-40vh) rotateX(-270deg)"
+    rotateX = -270
+})
