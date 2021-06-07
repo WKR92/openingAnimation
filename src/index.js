65a1a55a5545a1ef4c0f1f0dde30b5ef2fa3e04d
Wiktor <65229344+WKR92@users.noreply.github.com>
2021-06-07 17:06:05 +0200

    cosmic cube

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import debounce from 'lodash.debounce';
 gsap.registerPlugin(CSSRulePlugin);
 
 const rule = CSSRulePlugin.getRule(".box3:before");
+const underTopFaceRule = CSSRulePlugin.getRule(".under_face_top::after");
+
 
 const tl = gsap.timeline()
 
@@ -24,15 +26,94 @@ const tl = gsap.timeline()
 
 
     let rotateX = 0
-    const cube = document.querySelector('.cube');
-    cube.addEventListener('click', (event)=>{
-        event.preventDefault();
+    // const cube = document.querySelector('.cube');
+    // cube.addEventListener('click', (event)=>{
+    //     event.preventDefault();
         
-        cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX -90) + 'deg)';
-        rotateX = rotateX - 90;
-        console.log(rotateX);
+    //     cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX -90) + 'deg)';
+    //     rotateX = rotateX - 90;
+    //     console.log(rotateX);
+    // })
+
+    let isUnderFaceTopOpen = false
+    const cube = document.querySelector('.cube');
+    const faceTop = document.querySelector('.cube__face--top');
+    faceTop.addEventListener('click', (event)=>{
+        event.preventDefault();
+
+        const underFaceTop = document.querySelector('.under_face_top');
+        underFaceTop.style = 'transform: scaleX(1) rotateX( 90deg) translateZ(40vh)'
+
+
+        gsap.to(underTopFaceRule, {cssRule: {right: '100%'}, duration: .5, delay: .5})
+
+        isUnderFaceTopOpen = true
     })
 
+    const underFaceTop = document.querySelector('.under_face_top');
+    underFaceTop.addEventListener('click', ()=> {
+        underFaceTop.style = 'transform: scaleX(0) rotateX( 90deg) translateZ(40vh)'
+
+    //    setTimeout(()=>{
+    //     gsap.to(underTopFaceRule, {cssRule: {right: '0'}})
+    //    }, 500)
+
+        isUnderFaceTopOpen = false
+    })
+
+    let isUnderFaceBottomOpen = false
+    const faceBottom = document.querySelector('.cube__face--bottom');
+    faceBottom.addEventListener('click', (event)=>{
+        event.preventDefault();
+
+        const underFaceBottom = document.querySelector('.under_face_bottom');
+        underFaceBottom.style = 'transform: scaleX(1) rotateX(-90deg) translateZ(40vh)'
+
+
+        // gsap.to(underBottomFaceRule, {cssRule: {right: '100%'}, duration: .5, delay: .5})
+
+        isUnderFaceBottomOpen = true
+    })
+
+    const underFaceBottom = document.querySelector('.under_face_bottom');
+    underFaceBottom.addEventListener('click', ()=> {
+        underFaceBottom.style = 'transform: scaleX(0) rotateX(-90deg) translateZ(40vh)'
+
+    //    setTimeout(()=>{
+    //     gsap.to(underBottomFaceRule, {cssRule: {right: '0'}})
+    //    }, 500)
+
+        isUnderFaceBottomOpen = false
+    })
+
+
+    let isUnderFaceBackOpen = false
+    const faceBack = document.querySelector('.cube__face--back');
+    faceBack.addEventListener('click', (event)=>{
+        event.preventDefault();
+
+        const underFaceBack = document.querySelector('.under_face_back');
+        if(isUnderFaceBackOpen === false) {
+            underFaceBack.style.transform = 'scaleX(1) rotateY(180deg) rotate(180deg) translateZ(40vh)'
+            isUnderFaceBackOpen = true
+        } else {
+            underFaceBack.style.transform = 'scaleX(0) rotateY(180deg) rotate(180deg) translateZ(40vh)';
+            isUnderFaceBackOpen = false
+        }
+    })
+
+    const underFaceBack = document.querySelector('.under_face_back');
+    underFaceBack.addEventListener('click', ()=> {
+        underFaceBack.style.transform = 'scaleX(0) rotateY(180deg) rotate(180deg) translateZ(40vh)';
+        isUnderFaceBackOpen = false
+    })
+
+
+
+
+window.addEventListener('click', (e)=>{
+    console.log(e.target)
+})
 
     // working on scroll
     // let rotateX = 0
@@ -52,30 +133,55 @@ const tl = gsap.timeline()
     //     this.oldScroll = this.scrollY;
     // }
 
-//     function rotateCube(event) {
-//         event.preventDefault();
-//         console.log('start')
-//         const cube = document.querySelector('.cube'); 
-//         if (this.oldScroll < this.scrollY) {
-//             cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX - 90) + 'deg)';
-//             rotateX = rotateX - 90;
-//         } else {
-//             cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX + 90) + 'deg)';
-//             rotateX = rotateX + 90;
-//         }
-//         console.log(rotateX)
-//         this.oldScroll = this.scrollY;
+    function rotateCube(event) {
+        event.preventDefault();
+        console.log('start')
+        const cube = document.querySelector('.cube');
 
-//         console.log('koniec')
-//     }
 
-// window.addEventListener('scroll', throttle(rotateCube, 1000));
+        // zamykanie wysuniętych stron 
+        
+        if (isUnderFaceTopOpen === true) {
+            const underFaceTop = document.querySelector('.under_face_top');
+            underFaceTop.style = 'transform: scaleX(0) rotateX( 90deg) translateZ(40vh)'      
+            isUnderFaceTopOpen = false
+        }
+
+        if (isUnderFaceBottomOpen === true) {
+            underFaceBottom.style = 'transform: scaleX(0) rotateX(-90deg) translateZ(40vh)'
+            isUnderFaceBottomOpen = false
+        }
+
+        if (isUnderFaceBackOpen === true) {
+            underFaceBack.style.transform = 'scaleX(0) rotateY(180deg) rotate(180deg) translateZ(40vh)';
+            isUnderFaceBackOpen = false
+        }
+
+
+
+
+
+        if (this.oldScroll <= this.scrollY) {
+            cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX - 90) + 'deg)';
+            rotateX = rotateX - 90;
+        } else {
+            cube.style.transform = 'translateZ(-40vh) rotateX(' + (rotateX + 90) + 'deg)';
+            rotateX = rotateX + 90;
+        }
+        console.log(rotateX)
+        this.oldScroll = this.scrollY;
+
+        console.log('koniec')
+    }
+
+window.addEventListener('scroll', debounce(rotateCube, 50));
 
 
 
 let contactLeftValue = "100%"
 const contactNavBtn = document.querySelector('.contactNavBtn');
 contactNavBtn.addEventListener('click', () => {
+    console.log(contactLeftValue)
 
     const contactContainer = document.querySelector('.contactContainer');
     if(contactLeftValue === "20%"){
@@ -109,28 +215,78 @@ resizeContactNavBtn.addEventListener('click', () => {
 
 const homeNavBtn = document.querySelector('.homeNavBtn');
 homeNavBtn.addEventListener('click', () => {
-    const cube = document.querySelector('.cube');
-    cube.style.transform = "translateZ(-40vh) rotateX(0deg)"
-    rotateX = 0
+
+    if(contactLeftValue === "20%"){
+        const contactContainer = document.querySelector('.contactContainer');
+        contactContainer.style['left'] = "100%";
+        contactLeftValue = "100%";
+
+        setTimeout(()=>{
+            const cube = document.querySelector('.cube');
+            cube.style.transform = "translateZ(-40vh) rotateX(0deg)"
+            rotateX = 0
+        }, 400)
+    } else {
+        const cube = document.querySelector('.cube');
+        cube.style.transform = "translateZ(-40vh) rotateX(0deg)"
+        rotateX = 0
+    }    
 })
 
 const processNavBtn = document.querySelector('.processNavBtn');
 processNavBtn.addEventListener('click', () => {
-    const cube = document.querySelector('.cube');
-    cube.style.transform = "translateZ(-50vh) rotateY(-90deg)"
+
+    if(contactLeftValue === "20%"){
+        const contactContainer = document.querySelector('.contactContainer');
+        contactContainer.style['left'] = "100%";
+        contactLeftValue = "100%";
+
+        setTimeout(()=>{
+            const cube = document.querySelector('.cube');
+            cube.style.transform = "translateZ(-50vh) rotateY(-90deg)"
+        }, 400)
+    } else {
+        const cube = document.querySelector('.cube');
+        cube.style.transform = "translateZ(-50vh) rotateY(-90deg)"
+    }    
 })
 
 const portfolioNavBtn = document.querySelector('.portfolioNavBtn');
 portfolioNavBtn.addEventListener('click', () => {
-    const cube = document.querySelector('.cube');
-    cube.style.transform = "translateZ(-50vh) rotateY(90deg)"
+
+    if(contactLeftValue === "20%"){
+        const contactContainer = document.querySelector('.contactContainer');
+        contactContainer.style['left'] = "100%";
+        contactLeftValue = "100%";
+
+        setTimeout(()=>{
+            const cube = document.querySelector('.cube');
+            cube.style.transform = "translateZ(-50vh) rotateY(90deg)"
+        }, 400)
+    } else {
+        const cube = document.querySelector('.cube');
+        cube.style.transform = "translateZ(-50vh) rotateY(90deg)"
+    }     
 })
 
 const whyWeNavBtn = document.querySelector('.whyWeNavBtn');
 whyWeNavBtn.addEventListener('click', () => {
-    const cube = document.querySelector('.cube');
-    cube.style.transform = "translateZ(-40vh) rotateX(-270deg)"
-    rotateX = -270
+
+    if(contactLeftValue === "20%"){
+        const contactContainer = document.querySelector('.contactContainer');
+        contactContainer.style['left'] = "100%";
+        contactLeftValue = "100%";
+
+        setTimeout(()=>{
+            const cube = document.querySelector('.cube');
+            cube.style.transform = "translateZ(-40vh) rotateX(-270deg)"
+            rotateX = -270
+        }, 400)
+    } else {
+        const cube = document.querySelector('.cube');
+        cube.style.transform = "translateZ(-40vh) rotateX(-270deg)"
+        rotateX = -270
+    }         
 })
 
 
@@ -138,26 +294,76 @@ whyWeNavBtn.addEventListener('click', () => {
 
 const resizeHomeNavBtn = document.querySelector('.resizeHomeNavBtn');
 resizeHomeNavBtn.addEventListener('click', () => {
-    const cube = document.querySelector('.cube');
-    cube.style.transform = "translateZ(-40vh) rotateX(0deg)"
-    rotateX = 0
+
+    if(resizeContactLeftValue === "0%"){
+        const contactContainer = document.querySelector('.contactContainer');
+        contactContainer.style['left'] = "100%";
+        resizeContactLeftValue = "100%";
+
+        setTimeout(()=>{
+            const cube = document.querySelector('.cube');
+            cube.style.transform = "translateZ(-40vh) rotateX(0deg)"
+            rotateX = 0
+        }, 400)
+    } else {
+        const cube = document.querySelector('.cube');
+        cube.style.transform = "translateZ(-40vh) rotateX(0deg)"
+        rotateX = 0
+    }
 })
 
 const resizeProcessNavBtn = document.querySelector('.resizeProcessNavBtn');
 resizeProcessNavBtn.addEventListener('click', () => {
-    const cube = document.querySelector('.cube');
-    cube.style.transform = "translateZ(-50vh) rotateY(-90deg)"
+
+    if(resizeContactLeftValue === "0%"){
+        const contactContainer = document.querySelector('.contactContainer');
+        contactContainer.style['left'] = "100%";
+        resizeContactLeftValue = "100%";
+
+        setTimeout(()=>{
+            const cube = document.querySelector('.cube');
+            cube.style.transform = "translateZ(-50vh) rotateY(-90deg)"
+        }, 400)
+    } else {
+        const cube = document.querySelector('.cube');
+        cube.style.transform = "translateZ(-50vh) rotateY(-90deg)"
+    } 
 })
 
 const resizePortfolioNavBtn = document.querySelector('.resizePortfolioNavBtn');
 resizePortfolioNavBtn.addEventListener('click', () => {
-    const cube = document.querySelector('.cube');
-    cube.style.transform = "translateZ(-50vh) rotateY(90deg)"
+
+    if(resizeContactLeftValue === "0%"){
+        const contactContainer = document.querySelector('.contactContainer');
+        contactContainer.style['left'] = "100%";
+        resizeContactLeftValue = "100%";
+
+        setTimeout(()=>{
+            const cube = document.querySelector('.cube');
+            cube.style.transform = "translateZ(-50vh) rotateY(90deg)"
+        }, 400)
+    } else {
+        const cube = document.querySelector('.cube');
+        cube.style.transform = "translateZ(-50vh) rotateY(90deg)"
+    } 
 })
 
 const resizeWhyWeNavBtn = document.querySelector('.resizeWhyWeNavBtn');
 resizeWhyWeNavBtn.addEventListener('click', () => {
-    const cube = document.querySelector('.cube');
-    cube.style.transform = "translateZ(-40vh) rotateX(-270deg)"
-    rotateX = -270
+    
+    if(resizeContactLeftValue === "0%"){
+        const contactContainer = document.querySelector('.contactContainer');
+        contactContainer.style['left'] = "100%";
+        resizeContactLeftValue = "100%";
+
+        setTimeout(()=>{
+            const cube = document.querySelector('.cube');
+            cube.style.transform = "translateZ(-40vh) rotateX(-270deg)"
+            rotateX = -270
+        }, 400)
+    } else {
+        const cube = document.querySelector('.cube');
+        cube.style.transform = "translateZ(-40vh) rotateX(-270deg)"
+        rotateX = -270
+    }
 })
